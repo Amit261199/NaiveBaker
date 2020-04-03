12ad8dbb845b1186340c35264d1a92816f1d3033
Janam Vaidya <janamvaidya080599@gmail.com>
2020-04-04 00:09:04 +0530

    Add files via upload

--- SEN Project/buildpantry/buildpantry.js
+++ SEN Project/buildpantry/buildpantry.js
@@ -55,10 +55,9 @@ function newElement() {
 }
 
 function openWin(){
-	window.open("Recipe_Display.html");
+  window.open("Recipe_Display.html");
 }
 
 function openWins(){
-	window.open("Recipe_Display.html");
+  window.open("Recipe_Display.html");
 }
-
