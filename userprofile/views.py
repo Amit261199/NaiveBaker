b7ae24d1ec0f9751218ed3de48ae2514d700c348
patelsoham78 <34511151+patelsoham78@users.noreply.github.com>
2020-04-05 01:13:38 +0530

    Update views.py

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def userprofilepage(request):
     return render(request,'userprofilepage.html')
+
+def signup_view(request):
+	if request.method=='POST':
+		form = UserCreationForm(request.POST )
+		if form.is_valid():
+			form.save()
+			return redirect('buildpantry:getRecipe')
+	else:
+		form = UserCreationForm()
+	context = {
+		'form' : form
+	}
+	return render(request,'user_create.html',context)
