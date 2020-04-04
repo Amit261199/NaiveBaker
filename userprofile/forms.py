a7461f54ac5327047c0fbf79b893db9f3e135209
patelsoham78 <34511151+patelsoham78@users.noreply.github.com>
2020-04-05 01:12:09 +0530

    Add files via upload

---
+++ userprofile/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class ProductForm(forms.ModelForm):
+	title		= forms.CharField(
+						widget = forms.TextInput(
+								attrs = {
+								"placeholder" : "my title"
+								}
+							)
+					)
+	email		= forms.EmailField()
